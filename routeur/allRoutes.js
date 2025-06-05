beaa3ee8344d24a340a7ab4cedea32692fb1bacb
Morgan Mesuron <mmesuron@gmail.com>
2025-06-05 15:23:29 +0200

    nouveau ccommit

--- routeur/allRoutes.js
+++ routeur/allRoutes.js
@@ -7,6 +7,7 @@ export const allRoutes = [
     new Route("/signup", "Inscription", "/pages/auth/signup.html"),
     new Route("/account", "Mon compte", "/pages/auth/account.html"),
     new Route("/editPassword", "Changement de mot de pase", "/pages/auth/editPassword.html"),
+    new Route("/allResa", "Vos réservations", "/pages/Réservations/allResa.html"),
 ]
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Quai Antique";
